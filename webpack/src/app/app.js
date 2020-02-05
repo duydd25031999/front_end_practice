41ef7603754eed279adc8423b5771fd5229929e5
Do Duc Duy <doducduy.vietnam@gmail.com>
2020-02-05 16:17:13 +0700

    deploy firebase

--- webpack/src/app/app.js
+++ webpack/src/app/app.js
@@ -1,25 +1,16 @@
-import { inputsAreValid } from "./utils/inputs-are-valid";
-import { parseInputs } from "./utils/parse-inputs";
-import { api } from './utils/api';
-import { AlertComponent } from './components/alert.component';
-import { AdditionComponent } from './components/addition.component';
+// import { inputsAreValid } from "./utils/inputs-are-valid";
+// import { parseInputs } from "./utils/parse-inputs";
+// import { api } from './utils/api';
+// import { AlertComponent } from './components/alert.component';
+// import { AdditionComponent } from './components/addition.component';
 
-const alertComponent = new AlertComponent();
-const additionComponent = new AdditionComponent();
+// const alertComponent = new AlertComponent();
+// const additionComponent = new AdditionComponent();
+
+import { FooterText } from "./components/FooterText.component";
+
+const footerTextComponent = new FooterText();
 
 export const run = () => {
-  alertComponent.hideErrors();
-
-  additionComponent.onClick(() => {
-    alertComponent.hideErrors();
-    const inputs = additionComponent.getInputs();
-    const parsedInputs = parseInputs(...inputs);
-    if (inputsAreValid(...parsedInputs)) {
-      const [numA, numB] = parsedInputs;
-      additionComponent.setResult(numA + numB);
-    } else {
-      additionComponent.setResult("");
-      alertComponent.handleAdditionError(inputs, parsedInputs);
-    }
-  });
+  footerTextComponent.addFocusEvent();
 };
